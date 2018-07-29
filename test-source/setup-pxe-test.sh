316e45ef94a0833cc88ecca69365ab6e552e87b3
James Medel <jamesmedel94@gmail.com>
2018-07-29 02:50:36 -0700

    Modified setup-ambari.sh script that this script creates by removing the prompting for password that gets passed to pssh

--- test-source/setup-pxe-test.sh
+++ test-source/setup-pxe-test.sh
@@ -36,6 +36,8 @@ yum install -y epel-release
 yum install -y wget
 yum install -y perl
 yum install -y git
+yum install -y pssh
+yum install -y sshpass
 yum install -y net-tools
 yum install -y openssl
 yum install -y shellinabox
@@ -123,7 +125,7 @@ printf "Network Range IP End: $SUBNET_RANGE_IP_END\n"
 STATIC_IP_START_BASE_NUM=$(atoi 0.0.0.20)
 SUBNET_STATIC_IP_BASE_NUM=$(( $SUBNET_NUM + $STATIC_IP_START_BASE_NUM ))
 SUBNET_STATIC_IP_BASE=$(itoa $SUBNET_STATIC_IP_BASE_NUM)
-printf "Subnet Staic IP Base: $SUBNET_STATIC_IP_BASE\n"
+printf "Subnet Static IP Base: $SUBNET_STATIC_IP_BASE\n"
 
 # Node1 Static IP
 NODE1_OFFSET=$(atoi 0.0.0.1)
@@ -452,6 +454,7 @@ EOF
 
 # Create a setup-ambari-test.sh script that will be stored on the HTTP server of PXE
 # for node1-sb client to download
+mkdir -p /var/www/html/centos7-install/scripts/
 tee -a /var/www/html/centos7-install/scripts/setup-ambari.sh << EOF
 #!/bin/bash
 
@@ -482,33 +485,35 @@ case "\$CHECK_IP" in
 
     # Copy and Send id_rsa.pub and authorized_keys files to each host
     # Reference: https://www.tecmint.com/copy-files-to-multiple-linux-servers/
-    printf "Copy and Send id_rsa.pub and authorized_keys files to each host\n"
-    sshpass -p "\$PSSH_PASSWD" pscp.pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -Av "~/.ssh/id_rsa.pub ~/.ssh/"
-    sshpass -p "\$PSSH_PASSWD" pscp.pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -Av "~/.ssh/authorized_keys ~/.ssh/"
+    printf "Copy and Send id_rsa.pub file to each host\n"
+    sshpass -p "\$PSSH_PASSWD" pscp.pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -Av ~/.ssh/id_rsa.pub ~/.ssh/
+    printf "Copy and Send authorized_keys file to each host\n"
+    sshpass -p "\$PSSH_PASSWD" pscp.pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -Av ~/.ssh/authorized_keys ~/.ssh/
 
-    printf "Set permissions ~/.ssh and authorized_keys on each host\n"
+    printf "Set permissions ~/.ssh on each host\n"
     sshpass -p "\$PSSH_PASSWD" pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -A -i "chmod 700 ~/.ssh"
+    printf "Set permissions authorized_keys on each host\n"
     sshpass -p "\$PSSH_PASSWD" pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -A -i "chmod 600 ~/.ssh/authorized_keys"
 
     # Done setting password-less SSH
     printf "Done Setting Up Password-less SSH for Ambari to later install HDP....\n"
 
     printf "3: Enable NTP on each node in Cluster and Browser Host\n"
-    pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -A -i "systemctl enable ntpd"
+    pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -i "systemctl enable ntpd"
 
     printf "Enable CHRONY on each node in Cluster and Browser Host\n"
-    pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -A -i "systemctl start chronyd"
-    pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -A -i "systemctl enable chronyd"
+    pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -i "systemctl start chronyd"
+    pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -i "systemctl enable chronyd"
 
     printf "4: Disable iptables on each host for Ambari to communicate with them\n"
-    pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -A -i "systemctl disable firewalld"
-    pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -A -i "service firewalld stop"
+    pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -i "systemctl disable firewalld"
+    pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -i "service firewalld stop"
 
     printf "5: Disable SELinux and PackageKit\n"
     # Temporarily disable SELinux
-    pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -A -i "setenforce 0"
+    pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -i "setenforce 0"
     # Permanently disable SELinux
-    # pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -A -i "perl -pi -e 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config"
+    # pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -i "perl -pi -e 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config"
 
     # Check on install host (Ambari Server) if path to refresh-packagekit.conf
     # exists, if not, then no need to disable it
@@ -522,7 +527,7 @@ case "\$CHECK_IP" in
 
     # Set umask to 022 since Ambari, HDP, HDF support that value
     printf "Set unmask to 0022 since Ambari, HDP, HDF support that value\n"
-    pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -A -i "umask 0022"
+    pssh -h /etc/pssh-hosts -l root -x "-o StrictHostKeyChecking=no" -i "umask 0022"
 
     # On Node1 Server Host, download Ambari Repo
     printf "6: Downloading Ambari 2.7 Repo\n"
@@ -530,13 +535,6 @@ case "\$CHECK_IP" in
     # Download Ambari Repo, yum install ambari-server should work cause it is local
     # 4. Place the ambari.repo file on the Ambari Server host
     wget http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.7.0.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
-    # 5. Edit the priorities.conf file to add the following values
-    echo "#!/bin/bash" | tee -a /edit_priorities.sh
-    echo "tee -a /etc/yum/pluginconf.d/priorities.conf << EOF" | tee -a /edit_priorities.sh
-    echo "[main]" | tee -a /edit_priorities.sh
-    echo "enabled=1" | tee -a /edit_priorities.sh
-    echo "gpgcheck=0" | tee -a /edit_priorities.sh
-    echo "EOF" | tee -a /edit_priorities.sh
 
     bash /edit_priorities.sh
 
@@ -545,7 +543,7 @@ case "\$CHECK_IP" in
 
     # Verify ambari is apart of the repolist before installing ambari-server
     HAS_AMBARI_REPO=\$(echo \$REPO_CONFIG | grep -oE '(^| )ambari-2.7.[0-9].[0-9]( |$)' | awk 'FNR == 1 {print $1}')
-    if [ "$HAS_AMBARI_REPO" = "ambari-2.7.0.0" ]; then
+    if [ "\$HAS_AMBARI_REPO" = "ambari-2.7.0.0" ]; then
       printf "7: Repo List has Ambari Repo, Installing ambari-server\n"
       yum install -y ambari-server
 
@@ -554,10 +552,9 @@ case "\$CHECK_IP" in
       ambari-server setup -s
 
       printf "Starting Ambari\n"
-      # yum install -y net-tools
       ambari-server start
       ambari-server status
-      # Now ambari UI should be reachable at: http://node1-sb.hortonworks.com:8080
+      # Now Ambari UI is reachable at: http://node1-sb.hortonworks.com:8080
     else
       printf "Repo List doesn't have Ambari Repo\n"
     fi
@@ -572,7 +569,7 @@ EOF
 # Append kickstart post section to begin preparing the cluster environment for
 # an ambari install
 tee -a /var/www/html/centos7-install/ks.cfg << EOF
-%post --log=~/ks.log
+%post --log=/root/ks.log
 
 printf "1. Preparing Cluster Environment....\n"
 # Adding static IPs and hostnames to hosts file of each node in the cluster
@@ -606,7 +603,7 @@ case "\$CHECK_IP" in
     echo "${node_ip[6]}" | tee -a /etc/pssh-hosts
     echo "${node_ip[7]}" | tee -a /etc/pssh-hosts
 
-    wget http://$IPADDRESS/centos7-install/scripts/setup-ambari.sh -O ~/setup-ambari-test.sh
+    wget http://$IPADDRESS/centos7-install/scripts/setup-ambari.sh -O ~/setup-ambari.sh
 
     # Set Hostname for Node1
     printf "Task 4.7: Permanently set hostname\n"
