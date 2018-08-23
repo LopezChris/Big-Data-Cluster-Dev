901fc5b91b47307fac147e79f5eac06bcefeb693
James Medel <jamesmedel94@gmail.com>
2018-08-23 09:04:23 -0700

    Files from archive directory were deleted cause they were moved back into assets folder on on base directory of the project

--- install/minnowboards/archive/reference-files/export-ambari-blueprint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# https://community.hortonworks.com/questions/83437/ambari-export-blueprint.html
-curl -H "X-Requested-By: ambari" \
--X GET \
--u admin:admin http://node1-sb.hortonworks.com:8080/api/v1/clusters/MarketingLab?format=blueprint \
-| tee -a blueprint-hdp3-0-data-access.json
